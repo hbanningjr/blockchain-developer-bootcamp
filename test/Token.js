f8987498e01b3d0030c90d43d9fc2c56bd6b6fa1
harvey m banning jr <hbanningjr@mac.mynetworksettings.com>
2026-04-02 14:15:18 -0400

    Added and passed token contract tests

--- test/Token.js
+++ test/Token.js
@@ -1,26 +1,32 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), "ether")
+}
 describe("Token", () => {
     let token
 
     beforeEach(async () => {
         //Fetch token from Blockchain
         const Token = await ethers.getContractFactory("Token")
-        token = await Token.deploy()
+        token = await Token.deploy("Dapp University", "DAPP", "1000000")
     })
 
     it("has correct name", async () => {
-        //Read token name
-        const name = await token.name()
-        //check that name is correct
-        expect(name).to.equal("Dapp University")
+        expect(await token.name()).to.equal("Dapp University")
     })
 
     it("has correct symbol", async () => {
-        //read token symbol
-        const symbol = await token.symbol()
-        //check that symbol is correct
-        expect(symbol).to.equal("DAPP")
+        expect(await token.symbol()).to.equal("DAPP")
     })
+
+    it("has correct decimals", async () => {
+        expect(await token.decimals()).to.equal("18")
+    })
+
+    it("has correct total supply", async () => {
+        expect(await token.totalSupply()).to.equal(tokens("1000000"))
+    })
+
 })
